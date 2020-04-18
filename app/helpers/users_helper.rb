be20730a38cb2f58f99e1e4072bc10ae4abfb902
k-shi <kzysh05@gmail.com>
2020-04-18 04:44:30 +0000

    user signup

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,9 @@
+module UsersHelper
+  
+  #メールアドレスに対して自分のアバター画像を登録する
+  def gravatar_url(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+  end
+end
